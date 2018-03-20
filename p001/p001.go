bbcabe74ebc915b7fb04e695152c30f02ab9155b
samvelm-github <37568725+samvelm-github@users.noreply.github.com>
2018-03-20 00:00:09 -0700

    add comment

--- p001/p001.go
+++ p001/p001.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
+// Find the sum of all the multiples of 3 or 5 below 1000
 func main() {
-	fmt.Println(run(1000))
+	fmt.Println(Run(50))
 }
 
-func run(limit int) int {
+func Run(limit int) int {
 	sum := 0
 	for i := 0; i < limit; i++ {
 		if i%3 == 0 || i%5 == 0 {
