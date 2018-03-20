89c4875769d87b8c8221b80d254e3c486deda172
samvelm-github <37568725+samvelm-github@users.noreply.github.com>
2018-03-19 23:59:48 -0700

    test for first problem

---
+++ p001/p001_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestRun(t *testing.T) {
+	cases := []struct {
+		in, want int
+	}{
+		{1000, 233168},
+		{100, 2318},
+		{50, 543},
+		{50, 5439},
+	}
+	for _, c := range cases {
+		got := Run(c.in)
+		if got != c.want {
+			t.Errorf("Run(%d) == %d, want %d", c.in, got, c.want)
+		}
+	}
+}
