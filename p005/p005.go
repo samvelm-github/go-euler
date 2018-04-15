bd4a9a2c980f248d4fc936662515d85f581dbe8f
samvelm-github <37568725+samvelm-github@users.noreply.github.com>
2018-04-14 22:01:37 -0700

    GoDoc for math behind lcm calculation

--- p005/p005.go
+++ p005/p005.go
@@ -14,6 +14,9 @@ func main() {
 }
 
 // ProblemFive - Solution for problem five from project euler
+// Least common multiplier for two numbers can be calculated as
+// lcm = n1 * n2 / lcd(n1, n2), where lcd is the least common
+// divizor for the same two numbers
 func ProblemFive(limit int) int {
 	var lcm = 1
 	for i := 2; i <= limit; i++ {
